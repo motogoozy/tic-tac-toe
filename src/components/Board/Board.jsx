b100f7f906a7b3d133a9cfc602ee03ce5f5509cb
Kyle Payne <kylepayne@numetric.com>
2021-11-20 22:58:44 -0700

    render 9 Cell components

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import './Board.css';
+import Cell from '../Cell/Cell';
 
 export default function Board() {
-  return <div className='board'></div>;
+  const renderCells = () => {
+    const cells = [];
+
+    for (let i = 0; i < 9; i++) {
+      cells.push(<Cell key={i} />);
+    }
+
+    return cells;
+  };
+  return <div className='board'>{renderCells()}</div>;
 }

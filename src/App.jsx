98c55e0aa0d1e6061ac1107a7d245f50b83902b6
Kyle Payne <kylepayne@numetric.com>
2021-11-20 22:16:00 -0700

    add board component, add css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Board from './components/Board/Board';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Board />
+    </div>
+  );
 }
 
 export default App;

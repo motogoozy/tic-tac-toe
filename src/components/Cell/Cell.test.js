6613abeaa19e26f20f8f4bd18d9f91caa6dc40cf
Kyle Payne <kylepayne@numetric.com>
2021-11-21 17:17:49 -0700

    refactor test to check item container div for hover class

--- src/components/Cell/Cell.test.js
+++ src/components/Cell/Cell.test.js
@@ -10,13 +10,13 @@ describe('Cell', () => {
 
   it('hover class gets applied to cell on hover', () => {
     component.simulate('mouseenter');
-    expect(component.find('.cell').hasClass('hover')).toBe(true);
+    expect(component.find('.item-container').hasClass('hover')).toBe(true);
   });
 
   it('hover class is removed on mouse leave', () => {
     component.simulate('mouseenter');
     component.simulate('mouseleave');
-    expect(component.find('.cell').hasClass('hover')).toBe(false);
+    expect(component.find('.item-container').hasClass('hover')).toBe(false);
   });
 
   it('adds player class on hover', () => {

b8dfcaeb4ab5132feb19112db2c107ea2d91b34f
Kyle Payne <kylepayne@numetric.com>
2021-11-21 18:08:53 -0700

    add player selection state & props

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,20 @@ import { PLAYERS } from './constants';
 
 function App() {
   const [currentPlayer, setCurrentPlayer] = useState(PLAYERS.X);
+  const [xSelections, setXSelections] = useState([]);
+  const [oSelections, setOSelections] = useState([]);
+
+  const onSelect = id => {
+    if (currentPlayer === PLAYERS.X) {
+      setXSelections(arr => [...arr, id]);
+    } else {
+      setOSelections(arr => [...arr, id]);
+    }
+  };
 
   return (
     <div className='App'>
-      <Board currentPlayer={currentPlayer} />
+      <Board currentPlayer={currentPlayer} onSelect={onSelect} xSelections={xSelections} oSelections={oSelections} />
     </div>
   );
 }
--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,12 +1,24 @@
 import './Board.css';
 import Cell from '../Cell/Cell';
 
-export default function Board({ currentPlayer }) {
+export default function Board({ currentPlayer, onSelect, xSelections, oSelections }) {
   const renderCells = () => {
     const cells = [];
 
     for (let i = 0; i < 9; i++) {
-      cells.push(<Cell key={i} id={i} currentPlayer={currentPlayer} />);
+      const xSelected = xSelections.includes(i);
+      const oSelected = oSelections.includes(i);
+
+      cells.push(
+        <Cell
+          key={i}
+          id={i}
+          currentPlayer={currentPlayer}
+          onSelect={onSelect}
+          xSelected={xSelected}
+          oSelected={oSelected}
+        />
+      );
     }
 
     return cells;

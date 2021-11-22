f927f506598e61024b8fc194dc1fb66562755acd
Kyle Payne <kylepayne@numetric.com>
2021-11-21 18:35:32 -0700

    test - passes selected prop if player selections includes cell id

--- src/components/Board/Board.test.js
+++ src/components/Board/Board.test.js
@@ -1,14 +1,25 @@
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import Board from './Board.jsx';
+import Cell from '../Cell/Cell.jsx';
 
 describe('Board', () => {
   let component;
+  const props = {
+    xSelections: [],
+    oSelections: [],
+  };
 
   beforeEach(() => {
-    component = mount(<Board xSelections={[]} oSelections={[]} />);
+    component = shallow(<Board {...props} />);
   });
 
   it('renders 9 cell components', () => {
-    expect(component.find('div.cell').length).toBe(9);
+    expect(component.find(Cell).length).toBe(9);
+  });
+
+  it('passes selected prop if player selections includes cell id', () => {
+    component = shallow(<Board {...props} xSelections={[3]} oSelections={[5]} />);
+    expect(component.find(Cell).at(3).props().xSelected).toBe(true);
+    expect(component.find(Cell).at(5).props().oSelected).toBe(true);
   });
 });

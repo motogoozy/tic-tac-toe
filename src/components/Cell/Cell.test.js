04290ab49c17af307d57d6ed50e0d7cd96384c29
Kyle Payne <kylepayne@numetric.com>
2021-11-21 17:10:39 -0700

    test - adds player class on hover, refactor tests

--- src/components/Cell/Cell.test.js
+++ src/components/Cell/Cell.test.js
@@ -1,21 +1,27 @@
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import Cell from './Cell.jsx';
 
 describe('Cell', () => {
   let component;
 
   beforeEach(() => {
-    component = mount(<Cell />);
+    component = shallow(<Cell />);
   });
 
   it('hover class gets applied to cell on hover', () => {
     component.simulate('mouseenter');
-    expect(component.find('div.hover').length).toBe(1);
+    expect(component.find('.cell').hasClass('hover')).toBe(true);
   });
 
   it('hover class is removed on mouse leave', () => {
     component.simulate('mouseenter');
     component.simulate('mouseleave');
-    expect(component.find('div.hover').length).toBe(0);
+    expect(component.find('.cell').hasClass('hover')).toBe(false);
+  });
+
+  it('adds player class on hover', () => {
+    component = shallow(<Cell currentPlayer={'x'} />);
+    component.simulate('mouseenter');
+    expect(component.find('.cell').hasClass('x')).toBe(true);
   });
 });
